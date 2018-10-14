d0599bbff1d3c9bf8556382b58a3218899bf4b20
Jason Phan <xormanatee2@gmail.com>
2018-10-13 22:31:13 -0500

    refac: output functions are now in here
    I figured it would be easier down the line if they're all in their own file.

---
+++ output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#include "output.h"
+
+void output(const char *ptext, const char *outfile) {
+    /* Handle plaintext output. */
+    if (outfile)
+        printf("outputted to a file.\n");
+    else
+        printf("%s\n", ptext);
+}
