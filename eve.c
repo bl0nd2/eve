e433a8a7e684b946cdfb1443b6d827c7790ce8aa
Jason Phan <xormanatee2@gmail.com>
2018-10-13 22:27:52 -0500

    refac: moved crypto and output functions out
    Only thing in eve.c now is parser functions and main().
    Calls to setup and run crypto functions now have parameters since they're in a different file.

--- eve.c
+++ eve.c
@@ -5,41 +5,122 @@
 #include <string.h>
 #include <argp.h>
 
-#define MOD(a,b) (((a % b) + b) % b)
+#include "crypto.h"
 
 /* Global variables */
 
 static int arg_shifts[25];
-static char *arg_infile = NULL;
-static char *arg_outfile = NULL;
-static char *arg_cipher = NULL;
+static char *arg_infile     = NULL;
+static char *arg_outfile    = NULL;
+static char *arg_cipher     = NULL;
+static char *arg_encode     = NULL;
+static char *arg_hash       = NULL;
 static char *arg_ciphertext = NULL;
-static const char *program_invocation_short_name = "eve";
-static const char *program_help_link = "https://github.com/bl0nd/eve";
 
 /* Prototypes */
+static int parse_opt(int key, char *arg, struct argp_state *state);
+static int opt_error();
+static int setup_shifts(char *arg);
 static int help(void);
 static int version(void);
-static int caesar(const char *infile);
-static int free_ciphers(char *plntxt);
-static int output(const char *plntxt);
-static int parse_opt(int key, char *arg, struct argp_state *state);
 
+
+/***********************************
+ *            Parser               *
+ ***********************************/
+static int parse_opt(int key, char *arg, struct argp_state *state) {
+    switch(key) {
+        case 'h':
+            return help();
+        case 900:
+            return version();
+        case 'i': {
+            arg_infile = arg;
+            break;
+        }
+        case 'o': {
+            arg_outfile = arg;
+            break;
+        }
+        case 's': {
+            setup_shifts(arg);
+            break;
+        }
+        case 901: {
+            arg_cipher = "caesar";
+            arg_ciphertext = arg;
+            break;
+        }
+    }
+
+    return 0;
+}
+
+static int setup_shifts(char *arg) {
+    int shift_count = 0;
+    char *token;
+
+    while( (token = strsep(&arg,",")) != NULL ) {
+        if (strcmp(token, "all") == 0) {
+            for (shift_count = 0; shift_count < 25; shift_count++)
+                arg_shifts[shift_count] = shift_count + 1;
+            break;
+        }
+        else if (atoi(token) > 0 && atoi(token) < 26)
+            arg_shifts[shift_count++] = atoi(token);
+        else {
+            puts("error: invalid shift");
+            exit(1);
+        }
+    }
+    
+    return 0;
+}
+
+static int opt_error() {
+    if (arg_cipher) {
+        if (!arg_ciphertext && !arg_infile) {
+            printf("error: missing ciphertext.\n");
+            return 1;
+        }
+        else if (arg_ciphertext && arg_infile) {
+            printf("error: option \"--%s\" must be empty if option "
+                   "\"--infile\" is specified\n", arg_cipher);
+            return 1;
+        }
+        else if (arg_shifts[0] == 0) {
+            printf("error: no shift(s) given.\n");
+            return 1;
+        }
+    }
+
+    if (arg_encode) {
+        printf("handling encoding...\n");
+        return 1;
+    }
+
+    if (arg_hash) {
+        printf("handling hash...\n");
+        return 1;
+    }
+
+    return 0;
+}
 
 static int help(void) {
-    printf("usage: %s [--version] [--help] <mode> ciphertext\n\n"
-           "Decode encrypted or encoded text.\n"
-           "\n   Cryptography options\n"
-           "\t    --caesar    Decode shift ciphers\n"
-           "\t-s, --shift     Specify shift(s) to be used\n"
-           "\n   General options\n"
-           "\t-i, --infile    Specify file to be read\n"
-           "\t-o, --outfile   Specify file to be written to\n"
-           "\t-h, --help      Show this help menu\n"
-           "\t    --version   Show program version\n"
-           "\nSee %s for details.\n"
-           , program_invocation_short_name
-           , program_help_link
+    printf("usage: eve [options] <mode>[=<ciphertext>]\n"
+           "\nModes:\n"
+           "  ciphers      --caesar\n"
+           "  encodings    --base64\n"
+           "  hashes       --md5\n"
+           "\nOptions:"
+           "\n   general\n"
+           "     -i, --infile  INFILE              Specify file to be read\n"
+           "     -o, --outfile OUTFILE             Specify file to be written to\n"
+           "     -h, --help                        Show this help menu\n"
+           "         --version                     Show program version\n"
+           "\n   cryptography\n"
+           "     -s, --shift   SHIFT[,SHIFT...]    Specify shift(s) to be used\n\n"
     );
 
     return 0;
@@ -50,127 +131,10 @@ static int version(void) {
     return 0;
 }
 
-static int caesar(const char *infile) {
-    if (arg_shifts[0] == 0) {
-        printf("error: no shift(s) given.\n");
-        return 1;
-    }
 
-    if (! infile) {
-        int i, *shift, ascii_value, start_value;
-        char *plaintext = malloc(sizeof(char) * strlen(arg_ciphertext));
-        if (plaintext == NULL) return 1;
-
-        /* Decode cipher in every shift given */
-        for (shift = arg_shifts; *shift != 0; shift++) {
-            for (i = 0; i < strlen(arg_ciphertext); i++) {
-                ascii_value = arg_ciphertext[i];
-
-                if (isalpha(ascii_value)) {
-                    start_value = isupper(ascii_value) ? 65 : 97;
-                    plaintext[i] = start_value + MOD((ascii_value - start_value - *shift), 26);
-                }
-                else
-                    plaintext[i] = ascii_value;
-            }
-            plaintext[strlen(arg_ciphertext)] = '\0';
-
-            output(plaintext);
-        }
-        free_ciphers(plaintext);
-    }
-    else {
-        if (arg_ciphertext) {
-            printf("error: option \"--caesar\" must be empty if option "
-                   "\"--infile\" is specified\n");
-            return 1;
-        }
-        else {
-            printf("TODO\n");
-        }
-    }
-
-    return 0;
-}
-
-static int free_ciphers(char *plntxt) {
-    /* Free plaintext strings in cipher functions. */
-    free(plntxt);
-    return 0;
-}
-
-static int output(const char *plntxt) {
-    /* Handle plaintext output. */
-    if (arg_infile) 
-        printf("outputted to a file\n.");
-    else
-        printf("%s\n", plntxt);
-
-    return 0;
-}
-
-static int parse_opt(int key, char *arg, struct argp_state *state) {
-    switch(key)
-    {
-        case 'h':
-            return help();
-
-        case 900:
-            return version();
-
-        case 'i':
-        {
-            arg_infile = arg;
-            break;
-        }
-
-        case 'o':
-        {
-            arg_outfile = arg;
-            break;
-        }
-
-        case 's':
-        {
-            int shift_count = 0;
-            char *token;
-
-            /* Get list of shifts */
-            while( (token = strsep(&arg,",")) != NULL ) {
-                if (strcmp(token, "all") == 0) {
-                    for (shift_count = 0; shift_count < 25; shift_count++)
-                        arg_shifts[shift_count] = shift_count + 1;
-                    break;
-                }
-                else if (atoi(token) == 0)
-                    continue;
-                else if (atoi(token) < 0 || atoi(token) > 25) {
-                    printf("error: invalid shift \"%s\"\n", token);
-                    return 1;
-                }
-                else {
-                    arg_shifts[shift_count++] = atoi(token);
-                }
-            }
-            break;
-        }
-
-        case 901:
-        {
-            arg_cipher = "caesar";
-            if (arg)
-                arg_ciphertext = arg;
-            else {
-                printf("error: missing ciphertext.\n");
-                return 1;
-            }
-            break;
-        }
-    }
-
-    return 0;
-}
-
+/***********************************
+ *              Main               *
+ ***********************************/
 int main(int argc, char *argv[]) {
     struct argp_option options[] = {
         {"help",    'h', 0,            0,                   "Show this help menu"},
@@ -187,9 +151,15 @@ int main(int argc, char *argv[]) {
 
     assert(r == 0);
 
+    if (opt_error())
+        exit(1);
+
     if (arg_cipher) {
-        if (strcmp(arg_cipher, "caesar") == 0)
-            caesar(arg_infile);
+        if (strcmp(arg_cipher, "caesar") == 0) {
+            char *ptext = setup_plaintext(arg_infile, arg_ciphertext);
+            run_caesar(ptext, arg_shifts, arg_ciphertext, arg_infile, arg_outfile);
+            free_plaintext(ptext);
+        }
     }
 }
 
