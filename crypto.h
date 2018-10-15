ef9692c51a2422454386250d47b5427eeecd71d4
Jason Phan <xormanatee2@gmail.com>
2018-10-14 22:46:33 -0500

    updat: run_caesar() and caesar() use pointers

--- crypto.h
+++ crypto.h
@@ -1,10 +1,12 @@
 #ifndef CRYPTO_H_
 #define CRYPTO_H_
 
+#include "eve.h"
+
 char *setup_plaintext(const char *infile, const char *ctext);
 int free_plaintext(char *ptext);
 int total_char_count(const char *infile);
-void run_caesar(char *ptext, int shifts[], char *ctext, char *infile, char *outfile);
-void caesar(char *ctext, char *ptext, const int space, const int *shift, char *outfile);
+void run_caesar(char *ptext, Namespace *parser);
+void caesar(char *ctext, char *ptext, const int space, const int *shift);
 
 #endif // CRYPTO_H_
